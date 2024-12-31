c031067f4492dca40d38ddd87fe5dd8991c99581
Umang Sharma <isharmaumang@gmail.com>
2024-12-31 19:34:54 +0530

    Add files via upload

---
+++ To-Do-List-BlockChain-NextJS/README.md
@@ -0,0 +1,25 @@
+# Decentralized Voting Application using NextJS
+
+This is a demo application to implement voting in solidity smart contract and backend API in NextJS. 
+
+
+
+
+
+
+## Installation
+
+
+
+```shell
+npm install
+```
+
+You first need to compile the contract and upload it to the blockchain network. Go to https://remix.ethereum.org/, and deploy the smart contract on your choice of blockchain. Copy and paste the abi and contract address along with rpc-url of the blockchain network in /Utils/config.js. Also get the private key from your metamask account. 
+
+
+Once you have done everthing above, simply run the following command to make the server running
+
+```shell
+npm run dev
+```
---
+++ To-Do-List-BlockChain-NextJS/Utils/config.js
@@ -0,0 +1,109 @@
+export const API_URL = "https://sepolia.infura.io/v3/32aee738d2d34c9b902e52d758a9e57d";
+export const PRIVATE_KEY = "1f3db83654d8a6635acecbf1fdd28d445b4f4cd964448588cd01e709aec11857";
+export const contractAddress = "0x5c4252a87c4bbC1F6eb17D6257f4597FBF4968C7";
+export const contractAbi = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_desc",
+				"type": "string"
+			}
+		],
+		"name": "addTask",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			}
+		],
+		"name": "markAsFinished",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "getAllTasks",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "desc",
+						"type": "string"
+					},
+					{
+						"internalType": "enum TaskToDo.TaskStatus",
+						"name": "status",
+						"type": "uint8"
+					}
+				],
+				"internalType": "struct TaskToDo.Task[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			}
+		],
+		"name": "getTask",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "enum TaskToDo.TaskStatus",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "tasks",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "desc",
+				"type": "string"
+			},
+			{
+				"internalType": "enum TaskToDo.TaskStatus",
+				"name": "status",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
